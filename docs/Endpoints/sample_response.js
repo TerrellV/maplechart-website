56f2a228463fc62a695ce63609748cf8ccc0ea65
terrell vest <terrell@terrellvest.com>
2023-03-12 21:59:55 -0400

    push

--- docs/Endpoints/sample_response.js
+++ docs/Endpoints/sample_response.js
@@ -8,20 +8,32 @@ export function SampleResponse({endpoint, show_line_numbers}) {
     let [{ json_str }, setState] = useState({ json_str: '' })
 
     useEffect(() => {
+
         console.log("use effect")
+        let isMounted = true
         endpoint = (endpoint[0] === "/") ? endpoint.slice(1) : endpoint
         endpoint = endpoint.replace(/^\/|\/$/, "")  // replace leading and trailing '/'s
 
         const filename = endpoint.replace(/\//g, '#')  // replace internal '/'s
         const json_path = `/data_responses/${filename}.json`
         console.log(`fetching local json file ${json_path}`)
+
         async function loadJson() {
             const r = await fetch(json_path)
-            console.log(r)
+            if (r.status === 200 & isMounted) {
+                const data = await r.json()
+                console.log(data)
+                setState(state => {
+                    return {json_str: JSON.stringify(data, null, 4)}
+                })
+            }
+
         }
 
         loadJson()
-
+        return () => {
+            isMounted = false;
+        }
 
 
     }, [endpoint])
@@ -30,7 +42,7 @@ export function SampleResponse({endpoint, show_line_numbers}) {
         <CodeBlock
             language="json"
             title="test"
-            showLineNumbers
+            showLineNumbers={false}
         >
             {json_str}
         </CodeBlock>
